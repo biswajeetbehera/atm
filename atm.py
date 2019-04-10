72a04e35e9b8c2370cda15d6fd9cf01e99185fd4
biswajeetbehera <beherabiswajeet3@gmail.com>
2019-04-11 00:52:37 +0530

    lastet commit

---
+++ atm.py
@@ -0,0 +1,14 @@
+Select your language
+ option
+  a english
+  b hindi
+  c marathi
+  d odiya
+
+  enter your 4 digit pin
+  enter your amount
+  please wait
+  avilable balance
+  please enter to continiue for another transaction & cancel to close
+  Donnot for get to remove your card.
+  thank you for banking.
